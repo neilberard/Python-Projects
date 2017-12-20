c67cd89670f48fd22d35920b5e16b7b565756d40
Neil <neilberard@gmail.com>
2017-12-19 23:46:58 -0800

    added

---
+++ Interop/pyside/basic_window.py
@@ -0,0 +1,17 @@
+import sys
+from PySide2 import QtGui, QtCore, QtWidgets
+
+
+def create_window():
+    win = QtWidgets.QMainWindow()
+    return win
+
+
+app = QtWidgets.QApplication(sys.argv)
+win = create_window()
+win.show()
+app.exec_()
+if __name__ == '__main__':
+    pass
+
+print 'this'
