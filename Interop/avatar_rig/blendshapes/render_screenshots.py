af7e753e8ea49d6fc68d30bca9c0373b1b65dc99
Neil <neilberard@gmail.com>
2017-12-22 12:11:23 -0800

    add offsets

--- Interop/avatar_rig/blendshapes/render_screenshots.py
+++ Interop/avatar_rig/blendshapes/render_screenshots.py
@@ -1,12 +1,21 @@
-import pymel.core as pm
+import pymel.core as pymel
 """Cycles through shapes in Face_Blnd node and renders out playblasts"""
 
 
 """CONSTANTS"""
-blendName = pm.aliasAttr('Face_Blnd', query=True)
-time = pm.currentTime()
-path = pm.mel.eval('getenv "AVATARS_V3_PERFORCE_DEPOT"')
+blendName = pymel.aliasAttr('Face_Blnd', query=True)
+time = pymel.currentTime()
+#path = pymel.mel.eval('getenv "AVATARS_V3_PERFORCE_DEPOT"')
+path = 'C:/Users/v-nebera/Desktop/strips_test'
 timeline_length = 200
+ctrl_grp = pymel.PyNode('Ctrls_Grp')
+
+
+def cycle_though_body_shape():
+    for i in ctrl_grp.Proportion.getEnums():
+        ctrl_grp.Proportion.set(i)
+        screen_shot(i)
+
 
 def cycle_blendshapes(reset=True):
 
@@ -17,7 +26,7 @@ def cycle_blendshapes(reset=True):
         if check_target(target_name):
             if reset:
                 print "reset!"
-                pm.setAttr('Face_Blnd.' + target_name, 0)
+                pymel.setAttr('Face_Blnd.' + target_name, 0)
             else:
                 screen_shot(target_name)
 
@@ -42,16 +51,11 @@ def check_target(target_name):
 
 def screen_shot(target_name):
     final_path = str(path) + "/ScreenShot/" + target_name
-    print final_path
 
-    pm.setAttr('Face_Blnd.' + target_name, 1)
-
-    pm.playblast(filename=final_path, startTime=0, endTime=0, format='qt', compression='h263',
-                 quality=100, viewer=False, framePadding=0, widthHeight=(1024, 1024), showOrnaments=False,
-                 clearCache=True, percent=100)
-
-    pm.setAttr('Face_Blnd.' + target_name, 0)
-
-cycle_blendshapes(reset=True)
+    pymel.playblast(filename=final_path, startTime=0, endTime=0, format='image', compression='png',
+                    quality=100, viewer=False, framePadding=0, widthHeight=(2000, 2000), showOrnaments=False,
+                    clearCache=True, percent=100)
 
 
+
+cycle_though_body_shape()
