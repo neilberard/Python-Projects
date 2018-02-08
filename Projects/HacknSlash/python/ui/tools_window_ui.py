f173f97d6e4e8c777fd90c3ebeee082b5a57d715
neilberard <neilberard@gmail.com>
2018-02-07 22:38:36 -0800

    inits

--- Projects/HacknSlash/python/ui/tools_window_ui.py
+++ Projects/HacknSlash/python/ui/tools_window_ui.py
@@ -1,32 +1,32 @@
 from PySide2 import QtCore, QtWidgets, QtGui
 
 
-class ToolsWindowUi(object):
-
-    def setupUi(self, ToolsWindow):
-        """
-        :param ToolsWindow: Subclass caller passing itself.
-        :return:
-        """
-        ToolsWindow.setObjectName("AnimationExporterUi")
-        ToolsWindow.resize(500, 500)
-
 
 class ToolsWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(ToolsWindow).__init__(self)
-        self.setObjectName("ToolsWIndow")
+        super(ToolsWindow, self).__init__()
+        self.setObjectName("ToolsWindow")
+        # self.resize(500, 500)
 
-def create_window():
-    return QtWidgets.QMainWindow()
+        self.container = QtWidgets.QWidget(self)
+        self.layout = QtWidgets.QHBoxLayout(self.container)
+        self.container.setLayout(self.layout)
+        self.textbox = QtWidgets.QLineEdit(self.container)
+        self.layout.addWidget(self.textbox)
+
+
+        # self.verticalLayout = QtWidgets.QVBoxLayout(self)
+
+
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-    win = create_window()
+    win = ToolsWindow()
     win.show()
     app.exec_()
 
-#C:\Program Files\Autodesk\Maya2017\bin>mayapy E:\Python_Projects\Projects\HacknSlash\python\ui\tools_window_ui.py
+#C:\Program Files\Autodesk\Maya2017\bin\mayapy E:\Python_Projects\Projects\HacknSlash\python\ui\tools_window_ui.py
 
 
 
