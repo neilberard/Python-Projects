1869278a869668b6ddf2951b582ee01d09db11f3
neilberard <neilberard@gmail.com>
2018-02-17 14:02:51 -0800

    inits

--- Projects/HacknSlash/python/ui/tools_window.py
+++ Projects/HacknSlash/python/ui/tools_window.py
@@ -6,11 +6,15 @@ import maya.OpenMayaUI as omui
 import os
 import logging
 
+from Projects.HacknSlash.python.ui import rename_tools_window
+from Projects.HacknSlash.python.project.libs import build_fk_ctrls
+
+
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 # Load UI File
-ui_path = ui_file_name = os.path.dirname(__file__) + r'\rig_tools.ui'
+ui_path = ui_file_name = os.path.dirname(__file__) + r'\nb_tools.ui'
 FormClass, BaseClass = loadUiType(ui_file_name)
 
 
@@ -28,9 +32,20 @@ class ToolsWindow(QtWidgets.QMainWindow, FormClass):
         log.info('on_btn_unlock_attr_clicked, CLICKED')
 
     @QtCore.Slot()
-    def on_btn_unlock_attr_clicked(self):
+    def on_btn_rename_clicked(self):
+        rename_tools_window.showUI()
         log.info('on_btn_unlock_attr_clicked, CLICKED')
 
+
+def showUI():
+    for widget in QtWidgets.QApplication.allWidgets():
+        if type(widget).__name__ == ToolsWindow.__name__:
+            try:
+                widget.close()
+            except:
+                pass
+    rename_tools_window = ToolsWindow()
+    rename_tools_window.show()
 
 
 """Test Code"""
@@ -42,7 +57,9 @@ class ToolsWindow(QtWidgets.QMainWindow, FormClass):
 
 #C:\Program Files\Autodesk\Maya2017\bin\mayapy E:\Python_Projects\Projects\HacknSlash\python\ui\tools_window.py
 
-# from Projects.HacknSlash.python.ui import tools_window_ui
+# from Projects.HacknSlash.python.ui import tools_window
+# tools_window.showUI()
+
 # reload(tools_window_ui)
 # win = tools_window_ui.ToolsWindow()
 # win.show()
