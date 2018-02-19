203eebb3c6f99e16f61cee85d10c69ee1cebe0d3
neilberard <neilberard@gmail.com>
2018-02-19 15:08:00 -0800

    inits

---
+++ Projects/HacknSlash/python/project/libs/consts.py
@@ -0,0 +1,20 @@
+'''HS_Consts'''
+
+torso = {'pelvis': 'Pelvis', 'spine': 'Spine', 'chest': 'Chest', 'clavicle': 'Clavicle',
+         'foot': 'Foot', 'hand': 'Hand', 'pole': 'Pole',
+         'main': 'Main', 'switch': 'Switch', 'ikfk': 'IKFK',
+         }
+
+
+side = {'l': 'L', 'r': 'R'}
+
+arm = {'shoulder': 'Shoulder', 'elbow': 'Elbow', 'wrist': 'Wrist'}
+
+leg = {'hip': 'Hip', 'knee': 'Knee', 'ankle': 'Ankle', 'ball': 'Ball', 'toe': 'Toe'}
+
+hand = {'thumb': 'Thumb', 'index': 'Index', 'middle': 'Middle', 'ring': 'Ring', 'pinky': 'Pinky'}
+
+
+type = {'ik': 'IK', 'fk': 'FK', 'ctrl': 'CTRL', 'grp': 'GRP', 'hdl': 'HDL',
+        'arm': 'Arm', 'leg': 'Leg', 'offset': 'Offset', 'anno': 'Annotation', 'loc': 'LOC', 'utility': 'Utility',
+        'con': 'Condition', 'const': 'Constraint'}
--- Projects/HacknSlash/python/project/libs/naming_utils.py
+++ Projects/HacknSlash/python/project/libs/naming_utils.py
@@ -1,5 +1,22 @@
 
 
-
 class item_info(object):
-    pass
+    def __init__(self, str_name):
+        """
+        :param str_name: Name of the object to gather info.
+        """
+        self.name_list = str_name.split('_')
+        self._side = []
+
+    @property
+    def side(self):
+        return self._side
+
+    @side.setter
+    def side(self, value):
+        self.side = value
+
+
+
+
+
