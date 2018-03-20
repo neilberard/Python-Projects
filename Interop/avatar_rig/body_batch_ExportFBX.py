9a97e3a7dd7886f060583f5a209adf17a38ad050
Neil <neilberard@gmail.com>
2018-03-20 12:09:27 -0700

    got it working, use shape node

--- Interop/avatar_rig/body_batch_ExportFBX.py
+++ Interop/avatar_rig/body_batch_ExportFBX.py
@@ -1,37 +1,52 @@
-import pymel.core as pm
+import pymel.core as pymel
+from project.maya.lib.avatar import lib_av_naming
+from project.maya.lib.avatar import av_tool_consts
 
-i = []
 
-ctrl = pm.ls('Ctrls_Grp')
-
-b = pm.listRelatives('Body_Grp')
-ctrl = pm.ls('Ctrls_Grp')
+body_meshes = pymel.listRelatives('Body_Grp')
 
 
 def export_fbx():
 
-    for i in b:
-        if i.visibility.get():
-            pm.select(i, r=True)
-            pm.select('Head_Mesh', add=True)
+    for mesh in body_meshes:
 
-            if i.find('_Body_M_Mesh') != -1:
-                x = i.replace('_Body_M_Mesh', '_M.fbx')
-            if i.find('_Body_F_Mesh') != -1:
-                x = i.replace('_Body_F_Mesh', '_F.fbx')
+        info = lib_av_naming.Asset_Name(mesh)
 
-            path = 'C:/Users/v-nebera/avatars_p4/rig/bodyshapes_FBX/' + x
-            print path
-            pm.mel.FBXExport(s=True, f=path)
+        if info.proportion == 'Base':
+            continue
+
+        head_name = lib_av_naming.concatenate_str_from_list(['Head',
+                                                             info.proportion,
+                                                             info.gender,
+                                                             ])
+
+        head = pymel.PyNode(head_name)
+
+        try:
+            mesh.visibility.disconnect()
+            mesh.visibility.set(1)
+            head.visibility.set(1)
+        except:
+            pass
+
+        pymel.select(mesh, r=True)
+        pymel.select(head_name, add=True)
+
+        file_name = []
+
+        if mesh.find('_Body_M_Mesh') != -1:
+            file_name = mesh.replace('_Body_M_Mesh', '_M.fbx')
+        if mesh.find('_Body_F_Mesh') != -1:
+            file_name = mesh.replace('_Body_F_Mesh', '_F.fbx')
+
+        path = 'C:/Users/v-nebera/avatars_p4/rig/bodyshapes_FBX/' + file_name
+
+        pymel.mel.FBXExport(s=True, f=path)
 
 
-for i in range(14):
-    ctrl[0].Proportion.set(i)
+if __name__ == '__main__':
     export_fbx()
 
-for i in range(14):
-    ctrl[0].Proportion.set(i)
-    export_fbx()
-            
+
 
 
