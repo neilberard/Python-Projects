6c15822ffc73d821003eae4a819ef2417a8d00c6
Neil <neilberard@gmail.com>
2018-04-24 11:04:00 +0300

    It works!!!!

--- plug-ins/make_planer.py
+++ plug-ins/make_planer.py
@@ -16,19 +16,33 @@ def get_average_normal(face_selection):
 
 
 def get_plane_normal(average_pos, vertices):
+    vector_a = average_pos - OpenMaya.MVector(vertices[0].getPosition())
+    vector_b = average_pos - OpenMaya.MVector(vertices[-1].getPosition())
+
+    first_normal = vector_a ^ vector_b
+
     average_normal = OpenMaya.MVector(0, 0, 0)
-    count = 0
 
     for idx, vtx in enumerate(vertices):
 
-        vector_a = OpenMaya.MVector(vertices[idx-1].getPosition()) - average_pos
-        vector_b = OpenMaya.MVector(vertices[1].getPosition()) - average_pos
+        vector_a = average_pos - OpenMaya.MVector(vertices[idx].getPosition())
+        vector_b = average_pos - OpenMaya.MVector(vertices[idx-1].getPosition())
 
-        count += 1
-        average_normal += vector_a ^ vector_b
-    print count
-    if count:
-        average_normal /= count
+        normal = vector_a ^ vector_b
+        dot = normal * first_normal
+
+        if dot < 0:
+            normal = vector_b ^ vector_a
+
+        # final_pos = average_pos + normal
+        # pymel.spaceLocator(position=(final_pos.x, final_pos.y, final_pos.z))
+
+        average_normal += normal
+
+    print len(vertices)
+    if len(vertices):
+        average_normal /= len(vertices)
+
     average_normal.normalize()
 
     return average_normal
@@ -44,13 +58,13 @@ def get_average_pos(border_vertex_selection):
         pos = vtx.getPosition()
         average_pos += OpenMaya.MVector(pos[0], pos[1], pos[2])
 
-    print count
-
     average_pos /= count
     return average_pos
 
 
 def project_vertex():
+
+    # pymel.delete(pymel.ls(type='locator'))  # todo: TEMP
 
     # Get selected Object
     mesh = pymel.selected(objectsOnly=True)[0]
@@ -68,13 +82,11 @@ def project_vertex():
 
     pymel.runtime.ConvertSelectionToVertexPerimeter()
     pos = get_average_pos(pymel.selected(flatten=True))
-    # norm = get_plane_normal(pos, [pymel.PyNode('pSphere1.vtx[293]'), pymel.PyNode('pSphere1.vtx[298]')])
-    norm = get_average_normal(pymel.selected(flatten=True))
-    # norm = OpenMaya.MVector(0,1,0)
-
+    norm = get_plane_normal(pos, pymel.selected(flatten=True))
     pymel.select(sel)
+
     pymel.runtime.ConvertSelectionToVertices()
-    vert_selection = pymel.selected(flatten=True)
+    vert_selection = pymel.selected(flatten=True, )
     pymel.runtime.ConvertSelectionToFaces()
     pymel.select(sel)
 
@@ -92,8 +104,12 @@ def project_vertex():
 
         magnitude_squared = u * u
 
-        projection_scalar = dotProduct / magnitude_squared
-        projection = u * projection_scalar
+        if magnitude_squared:
+            projection_scalar = dotProduct / magnitude_squared
+            projection = u * projection_scalar
+
+        else:
+            projection = u
 
         final = vertexPos - projection
 
@@ -113,17 +129,19 @@ class MyUndoableCommand(OpenMayaMPx.MPxCommand):
         self.mfn = None
         self.points = None
         self.new_points = None
+        self.sel = None
 
     def doIt(self, args):
         """This is called once when the command is first executed"""
         self.redoIt()
 
     def redoIt(self):
-
+        self.sel = pymel.selected()
         self.mfn, self.points, self.new_points = project_vertex()
         self.mfn.setPoints(self.new_points)
 
     def undoIt(self):
+        pymel.select(self.sel)
         self.mfn.setPoints(self.points)
 
     def isUndoable(self):
