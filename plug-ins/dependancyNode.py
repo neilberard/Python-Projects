42bfdff486f7769f9db04ce7be87bbf977982f42
neilberard <neilberard@gmail.com>
2018-11-24 18:56:44 -0800

    added reset vertex position

---
+++ plug-ins/dependancyNode.py
@@ -0,0 +1,40 @@
+import sys
+import maya.OpenMaya as OpenMaya
+import maya.OpenMayaMPx as OpenMayaMpx
+
+kPluginNodeName = 'ribbonMesh'
+kPluginNodeId = OpenMaya.MTypeId(0x8700B)
+
+class curveMesh(OpenMayaMpx.MPxNode):
+    def __init__(self):
+        OpenMayaMpx.MPxNode.__init__(self)
+
+    def createMesh(self):
+        pass
+
+    def compute(self):
+        pass
+
+def nodeCreator():
+    return OpenMayaMpx.asMPxPtr(curveMesh())
+
+def nodeInitializer():
+    """
+    Add attrs
+    """
+def initializePlugin(mobject):
+    mplugin = OpenMayaMpx.MFnPlugin(mobject)
+
+    try:
+        mplugin.registerNode(kPluginNodeName, kPluginNodeId, nodeCreator, nodeInitializer)
+    except Exception as ex:
+        sys.stderr.write("Failed to register node {}: {}".format(kPluginNodeName, ex))
+        raise
+
+def uninitializePlugin(mobject):
+    mplugin = OpenMayaMpx.MFnPlugin(mobject)
+    try:
+        mplugin.deregisterNode(kPluginNodeId)
+    except Exception as ex:
+        sys.stderr.write("Failed to derigister node {}: {}".format(kPluginNodeName, ex))
+        raise
