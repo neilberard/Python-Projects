1899d7949dbef9df3fd84fe32e76250d94ba4612
neilberard <neilberard@gmail.com>
2019-01-01 20:50:17 -0800

    added reset vertex position

--- Interop/api2/poly_plane.py
+++ Interop/api2/poly_plane.py
@@ -2,7 +2,7 @@ import maya.api.OpenMaya as om2
 import maya.cmds as cmds
 
 
-def create_plane(uDivs=4, vDivs=100, curveName=None):
+def create_plane(width=2.0, uDivs=3, vDivs=100, curveName=None):
     """
     :param uDivs: faces width, min 1
     :param vDivs: faces height, min 1
@@ -11,34 +11,55 @@ def create_plane(uDivs=4, vDivs=100, curveName=None):
     if not curveName:
         return
 
+    up_vector = om2.MVector(0, 1.0, 0)
+
     selList = om2.MGlobal.getSelectionListByName('curve1')
     mfnCurve = om2.MFnNurbsCurve(selList.getDagPath(0))
-
 
     num_faces = uDivs * vDivs
 
     poly_counts = [4 for x in range(num_faces)]
     vertices = []
 
-    u_increment = 1.0
+    u_increment = width / uDivs
     v_increment = mfnCurve.length() / vDivs
+
+    u_offset = (width / 2.0) * -1
 
 
     for v in range(vDivs + 1):
 
+        # ================================================
+        # Curve Param
+        # ================================================
+        v_len = v_increment * v
+        v_param = mfnCurve.findParamFromLength(v_len)
+        v_curve_pos = mfnCurve.getPointAtParam(v_param, om2.MSpace.kWorld)
+
+        # ================================================
+        # Curve Direction
+        # ================================================
+        if v < vDivs:
+            v_len_b = v_len + v_increment
+            v_param_b = mfnCurve.findParamFromLength(v_len_b)
+            v_curve_pos_b = mfnCurve.getPointAtParam(v_param_b, om2.MSpace.kWorld)
+            dir_vector = om2.MVector(v_curve_pos_b) - om2.MVector(v_curve_pos)
+            dir_vector.normalize()
+            cross = up_vector ^ dir_vector
+
+        else:
+            v_len_b = v_len - v_increment
+            v_param_b = mfnCurve.findParamFromLength(v_len_b)
+            v_curve_pos_b = mfnCurve.getPointAtParam(v_param_b, om2.MSpace.kWorld)
+            dir_vector = om2.MVector(v_curve_pos_b) - om2.MVector(v_curve_pos)
+            dir_vector.normalize()
+            cross = dir_vector ^ up_vector
+
+
         for u in range(uDivs + 1):
-
-            u_pos = u_increment * u
-            v_pos = v_increment * v * -1.0
-
-            #================================================
-            # Curve Param
-            #================================================
-            v_len = v_increment * v
-            v_param = mfnCurve.findParamFromLength(v_len)
-            v_curve_pos = mfnCurve.getPointAtParam(v_param)
-
-            vertices.append(om2.MPoint(u_pos + v_curve_pos.x, 0.0, v_curve_pos.z, 0.0))
+            u_pos = (u_increment * u) + u_offset
+            final_pos = om2.MVector(v_curve_pos) + (cross * u_pos)
+            vertices.append(om2.MPoint(final_pos))
 
 
     poly_connects = []
@@ -52,7 +73,7 @@ def create_plane(uDivs=4, vDivs=100, curveName=None):
             c = b + uDivs + 1
             d = a + uDivs + 1
             poly_connects.extend([a, b, c, d])
-            print [a, b, c, d]
+
 
         start += (uDivs + 1)
 
@@ -60,8 +81,6 @@ def create_plane(uDivs=4, vDivs=100, curveName=None):
 
     plane = mesh.create(vertices, poly_counts, poly_connects)
 
-
-    print poly_connects
 
 
 create_plane(curveName='curve1')
